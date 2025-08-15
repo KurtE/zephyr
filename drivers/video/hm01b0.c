ede397a26a44f61b51627099d6407d9eda213a4d
Kurt Eckhardt <kurte@rockisland.com>
2025-08-15 13:10:12 -0700

    Experiments with the Hm01b0 code

--- drivers/video/hm01b0.c
+++ drivers/video/hm01b0.c
@@ -6,6 +6,7 @@
 
 #define DT_DRV_COMPAT himax_hm01b0
 
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/i2c.h>
 #include <zephyr/drivers/video-controls.h>
 #include <zephyr/drivers/video.h>
@@ -43,6 +44,236 @@ enum hm01b0_resolution {
 	RESOLUTION_320x240,
 	RESOLUTION_320x320,
 };
+
+//#define KURTE_HACKING
+#ifdef KURTE_HACKING
+/* Register set */
+/* Read only registers */
+#define MODEL_ID_H                  0x0000
+#define MODEL_ID_L                  0x0001
+#define FRAME_COUNT                 0x0005
+#define PIXEL_ORDER                 0x0006
+/* R&W registers */
+/* Sensor mode control */
+#define MODE_SELECT                 0x0100
+#define IMG_ORIENTATION             0x0101
+#define SW_RESET                    0x0103
+#define GRP_PARAM_HOLD              0x0104
+/* Sensor exposure gain control */
+#define INTEGRATION_H               0x0202
+#define INTEGRATION_L               0x0203
+#define ANALOG_GAIN                 0x0205
+#define DIGITAL_GAIN_H              0x020E
+#define DIGITAL_GAIN_L              0x020F
+/* Frame timing control */
+#define FRAME_LEN_LINES_H           0x0340
+#define FRAME_LEN_LINES_L           0x0341
+#define LINE_LEN_PCK_H              0x0342
+#define LINE_LEN_PCK_L              0x0343
+/* Binning mode control */
+#define READOUT_X                   0x0383
+#define READOUT_Y                   0x0387
+#define BINNING_MODE                0x0390
+/* Test pattern control */
+#define TEST_PATTERN_MODE           0x0601
+/* Black level control */
+#define BLC_CFG                     0x1000
+#define BLC_TGT                     0x1003
+#define BLI_EN                      0x1006
+#define BLC2_TGT                    0x1007
+/*  Sensor reserved */
+#define DPC_CTRL                    0x1008
+#define SINGLE_THR_HOT              0x100B
+#define SINGLE_THR_COLD             0x100C
+/* VSYNC,HSYNC and pixel shift register */
+#define VSYNC_HSYNC_PIXEL_SHIFT_EN  0x1012
+/* Automatic exposure gain control */
+#define AE_CTRL                     0x2100
+#define AE_TARGET_MEAN              0x2101
+#define AE_MIN_MEAN                 0x2102
+#define CONVERGE_IN_TH              0x2103
+#define CONVERGE_OUT_TH             0x2104
+#define MAX_INTG_H                  0x2105
+#define MAX_INTG_L                  0x2106
+#define MIN_INTG                    0x2107
+#define MAX_AGAIN_FULL              0x2108
+#define MAX_AGAIN_BIN2              0x2109
+#define MIN_AGAIN                   0x210A
+#define MAX_DGAIN                   0x210B
+#define MIN_DGAIN                   0x210C
+#define DAMPING_FACTOR              0x210D
+#define FS_CTRL                     0x210E
+#define FS_60HZ_H                   0x210F
+#define FS_60HZ_L                   0x2110
+#define FS_50HZ_H                   0x2111
+#define FS_50HZ_L                   0x2112
+#define FS_HYST_TH                  0x2113
+/* Motion detection control */
+#define MD_CTRL                     0x2150
+#define I2C_CLEAR                   0x2153
+#define WMEAN_DIFF_TH_H             0x2155
+#define WMEAN_DIFF_TH_M             0x2156
+#define WMEAN_DIFF_TH_L             0x2157
+#define MD_THH                      0x2158
+#define MD_THM1                     0x2159
+#define MD_THM2                     0x215A
+#define MD_THL                      0x215B
+#define STATISTIC_CTRL              0x2000
+#define MD_LROI_X_START_H           0x2011
+#define MD_LROI_X_START_L           0x2012
+#define MD_LROI_Y_START_H           0x2013
+#define MD_LROI_Y_START_L           0x2014
+#define MD_LROI_X_END_H             0x2015
+#define MD_LROI_X_END_L             0x2016
+#define MD_LROI_Y_END_H             0x2017
+#define MD_LROI_Y_END_L             0x2018
+#define MD_INTERRUPT                0x2160
+/*  Sensor timing control */
+#define QVGA_WIN_EN                 0x3010
+#define SIX_BIT_MODE_EN             0x3011
+#define PMU_AUTOSLEEP_FRAMECNT      0x3020
+#define ADVANCE_VSYNC               0x3022
+#define ADVANCE_HSYNC               0x3023
+#define EARLY_GAIN                  0x3035
+/*  IO and clock control */
+#define BIT_CONTROL                 0x3059
+#define OSC_CLK_DIV                 0x3060
+#define ANA_Register_11             0x3061
+#define IO_DRIVE_STR                0x3062
+#define IO_DRIVE_STR2               0x3063
+#define ANA_Register_14             0x3064
+#define OUTPUT_PIN_STATUS_CONTROL   0x3065
+#define ANA_Register_17             0x3067
+#define PCLK_POLARITY               0x3068
+/*
+ * Useful value of Himax registers
+ */
+#define HIMAX_RESET                 0x01
+#define PCLK_RISING_EDGE            0x00
+#define PCLK_FALLING_EDGE           0x01
+#define AE_CTRL_ENABLE              0x00
+#define AE_CTRL_DISABLE             0x01
+
+#define HIMAX_LINE_LEN_PCK_FULL     0x178
+#define HIMAX_FRAME_LENGTH_FULL     0x109
+
+#define HIMAX_LINE_LEN_PCK_FULL     0x178
+#define HIMAX_FRAME_LENGTH_FULL     0x109
+
+#define HIMAX_LINE_LEN_PCK_QVGA     0x178
+#define HIMAX_FRAME_LENGTH_QVGA     0x104
+
+#define HIMAX_LINE_LEN_PCK_QQVGA    0x178
+#define HIMAX_FRAME_LENGTH_QQVGA    0x084
+
+static const struct video_reg16 default_regs[] = {
+    {BLC_TGT,               0x08},          /*  BLC target :8  at 8 bit mode */
+    {BLC2_TGT,              0x08},          /*  BLI target :8  at 8 bit mode */
+    {0x3044,                0x0A},          /*  Increase CDS time for settling */
+    {0x3045,                0x00},          /*  Make symetric for cds_tg and rst_tg */
+    {0x3047,                0x0A},          /*  Increase CDS time for settling */
+    {0x3050,                0xC0},          /*  Make negative offset up to 4x */
+    {0x3051,                0x42},
+    {0x3052,                0x50},
+    {0x3053,                0x00},
+    {0x3054,                0x03},          /*  tuning sf sig clamping as lowest */
+    {0x3055,                0xF7},          /*  tuning dsun */
+    {0x3056,                0xF8},          /*  increase adc nonoverlap clk */
+    {0x3057,                0x29},          /*  increase adc pwr for missing code */
+    {0x3058,                0x1F},          /*  turn on dsun */
+    {0x3059,                0x1E},
+    {0x3064,                0x00},
+    {0x3065,                0x04},          /*  pad pull 0 */
+    {ANA_Register_17,       0x00},          /*  Disable internal oscillator */
+
+    {BLC_CFG,               0x43},          /*  BLC_on, IIR */
+
+    {0x1001,                0x43},          /*  BLC dithering en */
+    {0x1002,                0x43},          /*  blc_darkpixel_thd */
+    {0x0350,                0x7F},          /*  Dgain Control */
+    {BLI_EN,                0x01},          /*  BLI enable */
+    {0x1003,                0x00},          /*  BLI Target [Def: 0x20] */
+
+    {DPC_CTRL,              0x01},          /*  DPC option 0: DPC off   1 : mono   3 : bayer1   5 : bayer2 */
+    {0x1009,                0xA0},          /*  cluster hot pixel th */
+    {0x100A,                0x60},          /*  cluster cold pixel th */
+    {SINGLE_THR_HOT,        0x90},          /*  single hot pixel th */
+    {SINGLE_THR_COLD,       0x40},          /*  single cold pixel th */
+    {0x1012,                0x00},          /*  Sync. shift disable */
+    {STATISTIC_CTRL,        0x07},          /*  AE stat en | MD LROI stat en | magic */
+    {0x2003,                0x00},
+    {0x2004,                0x1C},
+    {0x2007,                0x00},
+    {0x2008,                0x58},
+    {0x200B,                0x00},
+    {0x200C,                0x7A},
+    {0x200F,                0x00},
+    {0x2010,                0xB8},
+    {0x2013,                0x00},
+    {0x2014,                0x58},
+    {0x2017,                0x00},
+    {0x2018,                0x9B},
+
+    {AE_CTRL,               0x01},          /*Automatic Exposure */
+    {AE_TARGET_MEAN,        0x64},          /*AE target mean          [Def: 0x3C] */
+    {AE_MIN_MEAN,           0x0A},          /*AE min target mean      [Def: 0x0A] */
+    {CONVERGE_IN_TH,        0x03},          /*Converge in threshold   [Def: 0x03] */
+    {CONVERGE_OUT_TH,       0x05},          /*Converge out threshold  [Def: 0x05] */
+    {MAX_INTG_H,            (HIMAX_FRAME_LENGTH_QVGA-2)>>8},          /*Maximum INTG High Byte  [Def: 0x01] */
+    {MAX_INTG_L,            (HIMAX_FRAME_LENGTH_QVGA-2)&0xFF},        /*Maximum INTG Low Byte   [Def: 0x54] */
+    {MAX_AGAIN_FULL,        0x04},          /*Maximum Analog gain in full frame mode [Def: 0x03] */
+    {MAX_AGAIN_BIN2,        0x04},          /*Maximum Analog gain in bin2 mode       [Def: 0x04] */
+    {MAX_DGAIN,             0xC0},
+
+    {INTEGRATION_H,         0x01},          /*Integration H           [Def: 0x01] */
+    {INTEGRATION_L,         0x08},          /*Integration L           [Def: 0x08] */
+    {ANALOG_GAIN,           0x00},          /*Analog Global Gain      [Def: 0x00] */
+    {DAMPING_FACTOR,        0x20},          /*Damping Factor          [Def: 0x20] */
+    {DIGITAL_GAIN_H,        0x01},          /*Digital Gain High       [Def: 0x01] */
+    {DIGITAL_GAIN_L,        0x00},          /*Digital Gain Low        [Def: 0x00] */
+
+    {FS_CTRL,               0x00},          /*Flicker Control */
+
+    {FS_60HZ_H,             0x00},
+    {FS_60HZ_L,             0x3C},
+    {FS_50HZ_H,             0x00},
+    {FS_50HZ_L,             0x32},
+
+    {MD_CTRL,               0x00},
+    {FRAME_LEN_LINES_H,     HIMAX_FRAME_LENGTH_QVGA>>8},
+    {FRAME_LEN_LINES_L,     HIMAX_FRAME_LENGTH_QVGA&0xFF},
+    {LINE_LEN_PCK_H,        HIMAX_LINE_LEN_PCK_QVGA>>8},
+    {LINE_LEN_PCK_L,        HIMAX_LINE_LEN_PCK_QVGA&0xFF},
+    {QVGA_WIN_EN,           0x01},          /* Enable QVGA window readout */
+    {0x0383,                0x01},
+    {0x0387,                0x01},
+    {0x0390,                0x00},
+    {0x3011,                0x70},
+    {0x3059,                0x02},
+    {OSC_CLK_DIV,           0x0B},
+    {IMG_ORIENTATION,       0x00},          /* change the orientation */
+    {0x0104,                0x01},
+    {0x0000,                0x00},  /* EOF */
+};
+
+static const struct video_reg16 himax_qvga_regs[] = {
+    {0x0383,                0x01},
+    {0x0387,                0x01},
+    {0x0390,                0x00},
+    {QVGA_WIN_EN,           0x01},// Enable QVGA window readout
+    {MAX_INTG_H,            (HIMAX_FRAME_LENGTH_QVGA-2)>>8},
+    {MAX_INTG_L,            (HIMAX_FRAME_LENGTH_QVGA-2)&0xFF},
+    {FRAME_LEN_LINES_H,     (HIMAX_FRAME_LENGTH_QVGA>>8)},
+    {FRAME_LEN_LINES_L,     (HIMAX_FRAME_LENGTH_QVGA&0xFF)},
+    {LINE_LEN_PCK_H,        (HIMAX_LINE_LEN_PCK_QVGA>>8)},
+    {LINE_LEN_PCK_L,        (HIMAX_LINE_LEN_PCK_QVGA&0xFF)},
+    {GRP_PARAM_HOLD,        0x01},
+    {0x0000,                0x00},  // EOF
+
+};
+
+
+#endif
 
 struct video_reg hm01b0_160x120_regs[] = {
 	{HM01B0_CCI_WIDTH, 0x3},
@@ -85,6 +316,12 @@ struct hm01b0_data {
 struct hm01b0_config {
 	const struct i2c_dt_spec i2c;
 	const uint8_t data_bits;
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios)
+	struct gpio_dt_spec reset;
+#endif
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(pwdn_gpios)
+	struct gpio_dt_spec pwdn;
+#endif
 };
 
 #define HM01B0_VIDEO_FORMAT_CAP(width, height, format)                                             \
@@ -111,6 +348,17 @@ static int hm01b0_apply_configuration(const struct device *dev, enum hm01b0_reso
 	const struct hm01b0_config *config = dev->config;
 	int ret;
 
+	printk("hm01b0_apply_configuration reset %p %u pwndn:%p %u\n", config->reset.port, config->reset.pin, 
+		config->pwdn.port, config->pwdn.pin);
+#ifdef KURTE_HACKING
+	if (resolution == RESOLUTION_320x240) {
+		ret = video_write_cci_multiregs16(&config->i2c, himax_qvga_regs, ARRAY_SIZE(himax_qvga_regs));
+		if (ret != 0) {
+			LOG_ERR("Error writing default registers (%d)", ret);
+			return ret;
+		}
+	}
+#else	
 	/* Number of registers is the same for all configuration */
 	ret = video_write_cci_multiregs(&config->i2c, hm01b0_init_regs[resolution],
 					ARRAY_SIZE(hm01b0_160x120_regs));
@@ -144,6 +392,7 @@ static int hm01b0_apply_configuration(const struct device *dev, enum hm01b0_reso
 		LOG_ERR("Failed to write GRP_PARAM_HOLD reg (%d)", ret);
 		return ret;
 	}
+#endif	
 	return ret;
 }
 
@@ -255,6 +504,7 @@ static int hm01b0_init(const struct device *dev)
 	struct hm01b0_data *data = dev->data;
 	const struct hm01b0_config *config = dev->config;
 	int ret;
+	LOG_INF("hm01b0_init called");
 
 	if (config->data_bits == 8) {
 		data->ctrl_val = 0x02;
@@ -267,10 +517,43 @@ static int hm01b0_init(const struct device *dev)
 		return -ENODEV;
 	}
 
+	LOG_INF("hm01b0_init check connection");
 	if (!hm01b0_check_connection(dev)) {
 		LOG_ERR("%s is not ready", dev->name);
 		return -ENODEV;
 	}
+	LOG_INF("hm01b0_init PWDN and REST pins");
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(pwdn_gpios)
+    /* Power up camera module */
+    if (config->pwdn.port != NULL) {
+        if (!gpio_is_ready_dt(&config->pwdn)) {
+            return -ENODEV;
+        }
+        ret = gpio_pin_configure_dt(&config->pwdn, GPIO_OUTPUT_ACTIVE);
+            if (ret < 0) {
+            LOG_ERR("Could not clear power down pin: %d", ret);
+            return ret;
+        }
+    }
+  gpio_pin_set_dt(&config->pwdn, 1);
+#endif
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios)
+    /* Reset camera module */
+    if (config->reset.port != NULL) {
+        if (!gpio_is_ready_dt(&config->reset)) {
+            return -ENODEV;
+        }
+        ret = gpio_pin_configure_dt(&config->reset, GPIO_OUTPUT_ACTIVE);
+        if (ret < 0) {
+            LOG_ERR("Could not set reset pin: %d", ret);
+            return ret;
+        }
+        /* Reset is active high, has 1ms settling time*/
+        k_msleep(1);
+        gpio_pin_set_dt(&config->reset, 0);
+        k_msleep(1);
+    }
+#endif
 
 	ret = hm01b0_soft_reset(dev);
 	if (ret != 0) {
@@ -278,10 +561,22 @@ static int hm01b0_init(const struct device *dev)
 		return ret;
 	}
 
+#ifdef KURTE_HACKING
+	ret = video_write_cci_multiregs16(&config->i2c, default_regs, ARRAY_SIZE(default_regs));
+	if (ret != 0) {
+		LOG_ERR("Error writing default registers (%d)", ret);
+		return ret;
+	}
+
+	video_write_cci_reg(&config->i2c, PCLK_POLARITY, (0x20 | PCLK_FALLING_EDGE));
+
+#endif
+
+
 	struct video_format fmt = {
 		.pixelformat = VIDEO_PIX_FMT_GREY,
-		.width = 160,
-		.height = 120,
+		.width = 320,
+		.height = 240,
 		.type = VIDEO_BUF_TYPE_OUTPUT,
 		.pitch = 160 * video_bits_per_pixel(VIDEO_PIX_FMT_GREY) / BITS_PER_BYTE,
 	};
@@ -294,9 +589,24 @@ static int hm01b0_init(const struct device *dev)
 	return 0;
 }
 
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(reset_gpios)
+#define HM01B0_RESET_GPIO(inst) .reset = GPIO_DT_SPEC_INST_GET_OR(inst, reset_gpios, {}),
+#else
+#define HM01B0_RESET_GPIO(inst)
+#endif
+
+#if DT_ANY_INST_HAS_PROP_STATUS_OKAY(pwdn_gpios)
+#define HM01B0_PWDN_GPIO(inst) .pwdn = GPIO_DT_SPEC_INST_GET_OR(inst, pwdn_gpios, {}),
+#else
+#define HM01B0_PWDN_GPIO(inst)
+#endif
+
 #define HM01B0_INIT(inst)                                                                          \
 	const struct hm01b0_config hm01b0_config_##inst = {                                        \
-		.i2c = I2C_DT_SPEC_INST_GET(inst), .data_bits = 1 /* Use only 1 pin for data */    \
+		.i2c = I2C_DT_SPEC_INST_GET(inst), 							\
+		.data_bits = DT_INST_PROP(0, data_bits), /* Use only 1 pin for data */    \
+		HM01B0_RESET_GPIO(inst)                 \
+		HM01B0_PWDN_GPIO(inst)        \
 	};                                                                                         \
 	struct hm01b0_data hm01b0_data_##inst;                                                     \
 	DEVICE_DT_INST_DEFINE(inst, &hm01b0_init, NULL, &hm01b0_data_##inst,                       \
